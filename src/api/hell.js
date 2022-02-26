5669014e24b3b8933084256ddc767820e7a6eedf
cxb <846402869@qq.com>
2022-02-26 11:23:08 +0800

    git 配置补充

---
+++ src/api/hell.js
@@ -0,0 +1,5 @@
+import {get} from "./request"
+
+export const sayHello =() =>{
+    return get("/hell0")
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,21 @@
 <script setup>
 import { ref } from 'vue'
+import {sayHello} from "../api/hell";
+const message = ref("时间刺客后台")
 
-defineProps({
-  msg: String
-})
+const he = () =>{
+  sayHello().then(req=>{
+    console.log(req)
+  })
+}
 
-const count = ref(0)
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ message }}</h1>
+  <q-btn color="primary" label="Primary" />
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
